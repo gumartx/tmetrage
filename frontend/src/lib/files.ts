bb46a1923e3b182f4796ba08927492d9d51a2995
Gustavo Gomes <gustavo.gmartx@gmail.com>
2026-05-18 21:17:37 -0300

    Hosp back/front

--- frontend/src/lib/files.ts
+++ frontend/src/lib/files.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 export function getImageUrl(path?: string | null) {
   if (!path) return undefined;
