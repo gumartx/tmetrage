385230f8d8c3b8ca644b2959b26ba686960c0620
Gustavo Gomes <gustavo.gmartx@gmail.com>
2026-05-20 00:11:53 -0300

    Fix: movieDTO

--- backend/src/main/java/com/gusmarg/tmetrage/dto/MovieDTO.java
+++ backend/src/main/java/com/gusmarg/tmetrage/dto/MovieDTO.java
@@ -2,6 +2,7 @@ package com.gusmarg.tmetrage.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gusmarg.tmetrage.entities.Movie;
 
@@ -10,24 +11,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class MovieDTO {
 
-	private Long id;
+    private Long id;
+
     @JsonProperty("title")
-	private String movieTitle;
+    private String movieTitle;
+
     @JsonProperty("poster_path")
-	private String posterPath;
+    private String posterPath;
+
+    @JsonProperty("genres")
     private List<GenreDTO> genres;
-    
+
     public MovieDTO(Movie entity) {
         this.id = entity.getId();
         this.movieTitle = entity.getTitle();
         this.posterPath = entity.getPosterPath();
-
         this.genres = entity.getGenres()
                 .stream()
                 .map(g -> new GenreDTO(g))
